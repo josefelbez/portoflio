77a4ea07e8daafdd3d3375fc301755a80a12d083
josef.elbez <josef.elbez@gmail.com>
2023-03-29 12:10:47 -0300

    fixed string error

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -4,6 +4,7 @@ import Link from "next/link"
 
 import { Josefin_Sans } from "next/font/google"
 import { CiMenuFries } from "react-icons/ci"
+import { BsArrowRight } from "react-icons/bs"
 import { GrClose } from "react-icons/gr"
 
 import { motion, LazyMotion, domAnimation, m } from "framer-motion"
@@ -17,7 +18,6 @@ const josefin = Josefin_Sans({
 
 export const Header = () => {
 
-
     const [menuOpen, setMenuOpen] = useState(false)
 
     useEffect(() => (
@@ -29,33 +29,38 @@ export const Header = () => {
     }
 
     return(
-        <header className="fixed top-0 left-0 w-full p-5 flex items-center justify-end sm:justify-center">
+        <header className="z-50 absolute top-0 left-0 w-full p-5 flex items-center justify-end sm:justify-center">
             <nav>
-                <ul className={`first:children:sm:hidden first:children:flex  ${josefin.className} flex children:hidden children:sm:flex gap-10 text-white uppercase children:border children:rounded-full children:p-3 children:border-transparent hover:children:border-white children:duration-75 children:ease-linear`}>
-                    <motion.li 
-                        whileHover={{ scale: 1.2, rotate: 90 }} 
-                        onClick={() => menuHandler()}
-                        className="text-2xl cursor-pointer"
-                    > 
+                <ul className={`first:children:sm:hidden first:children:flex ${josefin.className} flex children:hidden children:sm:flex gap-10 text-white uppercase first:children:rounded-full first:children:p-3 children:p-1 first:children:border children:border-b children:border-transparent hover:children:border-white children:duration-75 children:ease-linear`}>
+                    <motion.li whileHover={{ scale: 1.2, rotate: 90 }} onClick={() => menuHandler()}className="text-2xl cursor-pointer"> 
                         <CiMenuFries /> 
                     </motion.li>
                     <li> <Link href="">Home</Link> </li>
                     <li> <Link href="">Projects</Link> </li>
+                    <li> <Link href="">My Stack</Link> </li>
                     <li> <Link href="">About</Link> </li>
                 </ul>
                 <LazyMotion features={domAnimation}>
                     {menuOpen && (                    
-                        <m.div key="modal" initial={{opacity: 0, x:100}} animate={{opacity: 1, x:0}} exit={{x:100, opacity:0}} className="absolute inset-0 w-full h-screen bg-gradient-to-b from-zinc-600 to-zinc-900">
+                        <m.div key="modal" initial={{opacity: 0}} animate={{opacity: 1}} exit={{opacity:0}} className="z-50 absolute inset-0 w-full h-screen bg-gradient-to-b from-zinc-800 to-zinc-900">
                             <div className="flex items-center justify-between w-full p-5 text-white border-b">
                                 <h2 className="pl-3 font-bold">Menu</h2>
                                 <m.span 
                                 whileHover={{ scale: 1.2, rotate: 90 }} 
+                                initial={{rotate: 90}}
+                                animate={{rotate: 0}}
                                 onClick={() => menuHandler()}
                                 className="p-3 text-2xl cursor-pointer border border-transparent rounded-full hover:border-white"
                                 >
                                     <GrClose className="children:stroke-white"/>
                                 </m.span>
                             </div>
+                            <ul className="p-5 children:flex children:pr-2 children:children:items-center children:children:justify-between children:children:flex children:children:px-3 children:children:py-5 children:children:w-full">
+                                <li> <Link href=""> <span>Home</span> <span> <BsArrowRight/> </span> </Link> </li>
+                                <li> <Link href=""> <span>Projects</span> <span> <BsArrowRight/> </span> </Link> </li>
+                                <li> <Link href=""> <span>My Stack</span> <span> <BsArrowRight/> </span> </Link> </li>
+                                <li> <Link href=""> <span>About</span> <span> <BsArrowRight/> </span> </Link> </li>
+                            </ul>
                         </m.div>
                     )}
                 </LazyMotion>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,34 +4,84 @@ import Image from "next/image"
 
 import { Raleway } from 'next/font/google'
 
-import { motion, LazyMotion, domAnimation, m } from "framer-motion"
+import { LazyMotion, domAnimation, m } from "framer-motion"
+
+import {
+  motion,
+  useScroll,
+  useSpring,
+  useTransform,
+  MotionValue
+} from "framer-motion";
 
 const raleway = Raleway({
   subsets: ['latin'],
   weight: ['300', '400', '700']
 })
 
-import {BsGithub, BsLinkedin} from 'react-icons/bs'
+import { BsGithub, BsLinkedin } from 'react-icons/bs'
+import { CiCircleChevDown } from 'react-icons/ci'
+
 import Link from "next/link"
+import { Header } from "./components/Header"
+import { useRef } from "react";
+
+function useParallax(value: MotionValue<number>, distance: number) {
+  return useTransform(value, [0, 1], [-distance, distance]);
+}
 
 export default function Home() {
+  
+  const ref = useRef(null);
+
+  const { scrollYProgress } = useScroll({ target: ref });
+
+  const scaleX = useSpring(scrollYProgress, {
+    stiffness: 100,
+    damping: 30,
+    restDelta: 0.001
+  });
+
+  const y = useParallax(scrollYProgress, 300);
+
   return (
     <main className="bg-zinc-100">
-      <section className="px-5 h-screen w-full flex flex-col gap-10 justify-center items-center text-white bg-gradient-to-b from-zinc-600 to-zinc-900">
-        <LazyMotion features={domAnimation}>
-          <m.div initial={{ opacity: 0, scale: 0.5 }} animate={{ opacity: 1, scale: 1 }} className="rounded-full overflow-hidden aspect-square">
-            <Image className="max-w-[150px]  md:max-w-[250px] object-contain" alt="My Photo" src="/me.jpg" width={350} height={350} priority />
-          </m.div>
-        </LazyMotion>
-        <hgroup className={`${raleway.className} text-center space-y-5 md:space-y-10`}>
-          <h2 className="text-2xl md:text-5xl font-bold">Welcome to my portfolio!</h2>
-          <h3 className="text-md md:text-xl">My name is Josef and i'm a front end developer!</h3>
-        </hgroup>
-        <ul className="flex gap-5 text-2xl hover:children:text-zinc-300">
-          <li> <Link href="http://linkedin.com/in/josefelbez" target="_blank"> <BsLinkedin /> </Link> </li>
-          <li> <Link href="http://github.com/josef.elbez" target="_blank"> <BsGithub/> </Link> </li>
-        </ul>
+      <Header />
+      <section className="flex flex-col px-5 justify-center items-center gap-5 text-white">
+        <div className="flex flex-col gap-10 justify-center items-center h-screen">
+          <LazyMotion features={domAnimation}>
+            <m.div initial={{ opacity: 0, scale: 0.5 }} animate={{ opacity: 1, scale: 1 }} className="w-fit rounded-full overflow-hidden aspect-square">
+              <Image className="max-w-[150px] object-contain" alt="My Photo" src="/me.jpg" width={250} height={250} priority />
+            </m.div>
+          </LazyMotion>
+          <hgroup className={`${raleway.className} text-center space-y-5 md:space-y-5`}>
+            <h2 className="text-2xl md:text-4xl font-bold">Welcome to my portfolio!</h2>
+            <h3 className="text-md md:text-xl">My name is Josef and im a front end developer!</h3>
+          </hgroup>
+          <ul className="flex gap-5 text-2xl hover:children:text-zinc-300">
+            <li> <Link href="http://linkedin.com/in/josefelbez" target="_blank"> <BsLinkedin /> </Link> </li>
+            <li> <Link href="http://github.com/josef.elbez" target="_blank"> <BsGithub/> </Link> </li>
+          </ul>
+        </div>
+
+        <div className="mb-5 text-5xl mt-auto animate-bounce cursor-pointer ease-linear duration-75 rounded-full hover:bg-white hover:text-zinc-900">
+          <Link href="#2"> <CiCircleChevDown /> </Link>
+        </div>
       </section>
+
+      <section id="1" className="h-screen p-4 pb-0 flex flex-col relative text-white">
+        <div ref={ref} className="h-full bg-gradient-to-b from-zinc-800 to-zinc-900 p-4 flex justify-center text-white">
+          a
+        </div>
+      </section>
+
+      <section id="2" className="h-screen p-4 flex flex-col relative text-white">
+        <div ref={ref} className="h-full bg-gradient-to-b from-zinc-800 to-zinc-900 p-4 flex justify-center text-white">
+         b 
+        </div>
+      </section>
+      
+      <motion.div className="progress" style={{ scaleX }} />
     </main>
   )
 }
